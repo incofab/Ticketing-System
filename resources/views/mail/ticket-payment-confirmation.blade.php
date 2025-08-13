8a3c484f996923722d1d81b7554089cea1e7ae75
mac <mac@macs-MacBook-Pro.local>
2025-08-13 23:47:34 +0100

    fix

--- resources/views/mail/ticket-payment-confirmation.blade.php
+++ resources/views/mail/ticket-payment-confirmation.blade.php
@@ -4,19 +4,18 @@ Dear {{$ticketPayment->name}},
 
 Thank you for booking with us. Below are your event details:  
 
+Your payment has been confirmed.   
+
 @component('mail::panel')
+<div style="margin-bottom: 7px;"><b>Amount:</b> {{number_format($paymentReference->amount)}}</div>
+<div style="margin-bottom: 7px;"><b>Quantity:</b> {{number_format($paymentReference->quantity)}}</div>
+<div style="margin-bottom: 7px;"><b>Package:</b> {{$eventPackage->title}}</div>
 <div style="margin-bottom: 7px;"><b>Event:</b> {{$event->title}}</div>
 <div style="margin-bottom: 7px;"><b>Date:</b>&nbsp; {{$event->start_time->toFormattedDayDateString()}}</div>
-<div><b>Venue:</b> {{$event->venue}}</div>
+<div style="margin-bottom: 7px;"><b>Venue:</b> {{$event->venue}}</div>
 @endcomponent
-    
-Your payment has been confirmed.   
-<div style="margin-bottom: 7px;"><b>Amount:</b> {{number_format($paymentReference->amount)}}</div>
-<div style="margin-bottom: 7px;"><b>Quantity:</b> {{number_format($paymentReference->amount)}}</div>
-<div style="margin-bottom: 7px;"><b>Package:</b> {{$eventPackage->title}}</div>
 
 To view your ticket and confirm your attendance, RSVP at <a href="{{$callbackUrl}}">{{$callbackUrl}}</a> 
-
 
 If you found this email in your Spam folder, please "Report not Spam".  
 
